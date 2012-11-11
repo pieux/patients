842150a4ae4809ff68bc7138bf0b54c68a1e2b99
Pieux Xi <pieuxxi@gmail.com>
2012-11-11 17:26:48 +0800

    rails new & bi

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Patients::Application.config.secret_token = '74781e85b7f5d940edea064b1d0f3ae14e8572aeb43833f01855f542d27006676f238f14ab0c62dd2e1cfc6ac0d963256c3e4c7a27086c2db7898d5c8b82c659'
